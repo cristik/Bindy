259c0a22d2118f93168d2eb1e305c17f32abca59
Cristian Kocza <cristiankocza@192-168-0-101.rdsnet.ro>
2017-01-18 07:14:38 +0200

    Initial Commit

---
+++ Bindy/Bindy.h
@@ -0,0 +1,19 @@
+//
+//  Bindy.h
+//  Bindy
+//
+//  Created by Cristian Kocza on 18/01/2017.
+//  Copyright © 2017 cristik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bindy.
+FOUNDATION_EXPORT double BindyVersionNumber;
+
+//! Project version string for Bindy.
+FOUNDATION_EXPORT const unsigned char BindyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bindy/PublicHeader.h>
+
+
